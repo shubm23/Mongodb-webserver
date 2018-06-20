a8019e135f73c026dff3260cc0fc222a1b743654
shubm23 <shubm23@gmail.com>
2018-06-21 00:19:12 +0530

    Modified Server with get and post request

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-
+app.use('/todos', function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+});
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
@@ -17,11 +20,19 @@ app.post('/todos',(req,res)=>{
   todo.save().then((doc)=>{
     res.send(doc);
   },(e)=>{
-    res.send(e);
+    res.status(400).send(e);
   })
 });
-
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
 
 app.listen(4000, () => {
   console.log('Started on port 4000');
 });
+
+module.exports ={app};
