0818da5267fec7e1ca75cbb2d045a462a9d9f4e6
shubm23 <shubm23@gmail.com>
2018-06-22 13:25:49 +0530

    Authentication middleware

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var port = 4000;
+var port = 3000;
 var app = express();
+const jwt = require('jsonwebtoken');
+var {authentication} = require('./middleware/authentication');
 
 
 app.use('/todos', function (req, res, next) {
@@ -94,18 +96,23 @@ app.patch('/todos/:id',(req,res)=>{
 });
 //Authentication
 
-app.post('/login',(req,res)=>{
-    var body = _.pick(req.body,['email','password']);
-    var user = new User(body);
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
 
-    user.save().then(()=>{
-      return user.generateAuthToken();
-    }).then((token) => {
-      console.log(token);
-      res.header('x-auth', token).send(user);
-    }).catch((e)=>{
-      res.status(400).send(e);
-    })
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('X-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+//User/me
+
+app.get('/users/me', authentication, (req, res) => {
+  res.send(req.user);
 });
 
 //Listen Port
